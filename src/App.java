9eec0d89933d25347fb63d17373e9a67efae651e
Llanes_Antonio <llana692@hsd.k12.or.us>
2025-01-08 10:08:59 -0800

    Greeting the user with scanner

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        // Greet User
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Antonio's Grocery Store! Whats your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "! Let us start shoppin hm?");
+
+        // Instantiate Objects
+
+
+        // Print Objects
+
+
+
+
+
     }
 }
